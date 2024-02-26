e0933f79b886f35f7617f17e6f1cde4d541263e4
SouiRania <rania.souissi13@gmail.com>
2024-02-26 15:10:11 +0100

    Added the components Home, About and Contact

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import RecetteList from './RecetteList';
+
+
+function Home() {
+  return (
+    <div>
+      <RecetteList></RecetteList>
+    </div>
+  )
+}
+
+export default Home
